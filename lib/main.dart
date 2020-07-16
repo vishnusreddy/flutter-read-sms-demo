848fc88b2d45033d8797b990e8faf0d912035915
Vishnu S Reddy <52468309+Vishnu-S-Reddy@users.noreply.github.com>
2020-07-16 20:39:07 +0530

    Added Google Auth

--- lib/main.dart
+++ lib/main.dart
@@ -2,70 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:sms/sms.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'login_page.dart';
 
 void main() => runApp(MyApp());
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
 
-class _MyAppState extends State<MyApp> {
-  final GoogleSignIn googleSignIn = GoogleSignIn();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('G-Login'), actions: <Widget>[
-          FutureBuilder(
-            future: googleSignIn
-                .isSignedIn(), // Checks whether user is already logged in or not
-            builder: (_, AsyncSnapshot<bool> snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.data) {
-                  // already logged in
-                  return IconButton(
-                      icon: Icon(Icons.block),
-                      onPressed: () async {
-                        try {
-                          await googleSignIn.signOut();
-                          setState(() {});
-                        } catch (error) {}
-                      });
-                } else {
-                  // no user logged in
-                  return IconButton(
-                      icon: Icon(Icons.account_circle),
-                      onPressed: () async {
-                        await googleSignIn.signIn();
-                        setState(() {});
-                      });
-                }
-              } else {
-                return CircularProgressIndicator();
-              }
-            },
-          )
-        ]),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Text('hi ${googleSignIn.currentUser?.displayName}!!'),
-              FutureBuilder(
-                future: googleSignIn.currentUser?.authentication,
-                builder:
-                    (_, AsyncSnapshot<GoogleSignInAuthentication> snapShot) {
-                  return snapShot.connectionState == ConnectionState.done
-                      ? (snapShot.hasData
-                      ? Text('id-token, ${snapShot.data.idToken}')
-                      : Text('Ooops something went wrong'))
-                      : Text('loading....');
-                },
-              )
-            ],
-          ),
-        ),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: LoginPage(),
     );
   }
 }
