f8d5668c0a32b22dd5a8cbec7c5fbab77021b399
Vishnu S Reddy <52468309+Vishnu-S-Reddy@users.noreply.github.com>
2020-07-16 15:27:20 +0530

    sms done

--- lib/messages.dart
+++ lib/messages.dart
@@ -1,22 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:sms/sms.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: MyInbox(),
-    );
-  }
-}
 class MyInbox extends StatefulWidget{
   @override
   State createState() {
